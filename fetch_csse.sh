f5f8223364c631d24423149d257e1ddf8da62732
Tomas Gavenciak <gavento@gmail.com>
2020-03-13 08:37:16 +0100

    Add data fetching and example output

---
+++ fetch_csse.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+D=CSSE-COVID-19
+
+if [ -e "$D" ]; then
+    echo "Updating CSSE github"
+    cd "$D"
+    git pull
+    cd ..
+else
+    echo "Cloning CSSE github"
+    git clone https://github.com/CSSEGISandData/COVID-19 "$D"
+fi
