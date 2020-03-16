7302e7fdef46eb0482436327aebf74b4ea4e3a9d
Tomas Gavenciak <gavento@gmail.com>
2020-03-17 00:16:18 +0100

    Finish robust exception add/remove on mitigation setting

--- fttogv/gleamdef.py
+++ fttogv/gleamdef.py
@@ -16,26 +16,32 @@ class GleamDef:
         self.base_name = base_name or self.path.stem
         self.updated = datetime.datetime.now()
         self.updated_fmt = self.updated.strftime("%Y-%m-%d_%H:%M:%S")
-        self._mitigation = None
+        self._exceptions = self.f1('gv:definition/gv:exceptions')
+        self._mitigation_ex = self.f1('gv:definition/gv:exceptions/gv:exception[@continents="1 2 4 3 5"]')
+        self._mitigation_val = self._mitigation_ex.find('gv:variable[@name="beta"]', namespaces=self.ns)
+        assert self._mitigation_val is not None
 
     def full_name(self):
         return "{} {} {}".format(self.base_name, self.updated_fmt, self.fmt_params())
 
-    def get(self, path):
-        rs = self.root.findall(path, namespaces=self.ns)
-        if not len(rs) == 1:
-            raise Exception("Expected one XML object at path {!r}, found {!r}".format(path, rs))
-        return rs[0]
+    def fa(self, query):
+        return self.root.findall(query, namespaces=self.ns)
+
+    def f1(self, query):
+        x = self.root.findall(query, namespaces=self.ns)
+        if not len(x) == 1:
+            raise Exception("Expected one XML object at query {!r}, found {!r}".format(query, x))
+        return x[0]
 
     def save(self, newpath=None):
         if not newpath:
             newpath = self.path.parent / (self.full_name().replace(' ', '_') + ".xml")
-        self.get('./gv:definition').attrib['name'] = self.full_name()
+        self.f1('./gv:definition').attrib['name'] = self.full_name()
         self.tree.write(newpath)
         log.info("Written GleamViz XML to {!r} (updated from {!r})".format(newpath, self.path))
 
     def clear_seeds(self):        
-        self.get('./gv:definition/gv:seeds').clear()
+        self.f1('./gv:definition/gv:seeds').clear()
 
     def add_seeds(self, regions, est_key='est_active', compartments={"Infectious": 1.0}, top=None):
         regs = []
@@ -50,7 +56,7 @@ class GleamDef:
     
         rec(regions.root())
         regs.sort(key=lambda er: er[0], reverse=True)
-        sroot = self.get('./gv:definition/gv:seeds')
+        sroot = self.f1('./gv:definition/gv:seeds')
         for e, reg in regs[:top]:
             for com_n, com_f in compartments.items():
                 if com_f and e * com_f >= 1.0:
@@ -60,56 +66,37 @@ class GleamDef:
 
     @property
     def param_seasonality(self):
-        return float(self.get('./gv:definition/gv:parameters').attrib['seasonalityAlphaMin'])
+        return float(self.f1('./gv:definition/gv:parameters').attrib['seasonalityAlphaMin'])
 
     @param_seasonality.setter
     def param_seasonality(self, val):
         assert val <= 2.0
-        self.get('./gv:definition/gv:parameters').attrib['seasonalityAlphaMin'] = "{:.3f}".format(val)
+        self.f1('./gv:definition/gv:parameters').attrib['seasonalityAlphaMin'] = "{:.3f}".format(val)
 
     @property
     def param_air_traffic(self):
-        return float(self.get('./gv:definition/gv:parameters').attrib['occupancyRate']) / 100
+        return float(self.f1('./gv:definition/gv:parameters').attrib['occupancyRate']) / 100
 
     @param_air_traffic.setter
     def param_air_traffic(self, val):
-        assert val <= 2.0
-        self.get('./gv:definition/gv:parameters').attrib['occupancyRate'] = "{:.3f}".format(val * 100)
-
-    def get_mitigation_variable_node(self):
-        exs = self.root.findall('./gv:definition/gv:exceptions/gv:exception', namespaces=self.ns)
-        assert len(exs) >= 1
-        for e in exs:
-            if e.attrib['continents'] == "1 2 4 3 5":
-                for v in e.findall('./gv:variable', namespaces=self.ns):
-                    if v.attrib['name'] == 'beta':
-                        return e, v
-                return e, None
-        raise Exception("Global mitigation Exception node (with continents='1 2 4 3 5') not found")
+        assert val < 1.1
+        v = int(max(min(val * 100, 100), 0))
+        self.f1('./gv:definition/gv:parameters').attrib['occupancyRate'] = str(v)
 
     @property
     def params_mitigaton(self):
-        if self._mitigation is not None:
-            return self._mitigation
-        _en, vn = self.get_mitigation_variable_node()
-        if vn is not None:
-            return float(vn.attrib['value'])
-        else:
-            return 0.0  ### NOTE: Hacky? Better return None?
+        return float(self._mitigation_val.get('value'))
 
     @params_mitigaton.setter
     def params_mitigaton(self, val):
         assert val <= 2.0
-        self._mitigation = val
-        en, vn = self.get_mitigation_variable_node()
+        self._mitigation_val.set('value', "{:.3f}".format(val))
         if val == 0.0:
-            if vn is not None:
-                en.remove(vn)
-                vn = None
+            if self._mitigation_ex in self._exceptions:
+                self._exceptions.remove(self._mitigation_ex)
         else:
-            if vn is None:
-                vn = ET.SubElement(en, 'variable', {'name': 'beta'})
-            vn.attrib['value'] = "{:.3f}".format(val)
+            if self._mitigation_ex not in self._exceptions:
+                self._exceptions.append(self._mitigation_ex)
 
     def fmt_params(self):
         return "seasonality={:.3f} airtraffic={:.3f} mitigation={:.3f}".format(self.param_seasonality, self.param_air_traffic, self.params_mitigaton)
