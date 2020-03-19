71b4e2283085871b3a846d939922b3c8b4de0eb4
Tomas Gavenciak <gavento@gmail.com>
2020-03-19 13:14:53 +0100

    Minor fixes, update script test

--- epifor/regions.py
+++ epifor/regions.py
@@ -150,57 +150,6 @@ class Regions:
         Region._from_yaml(self, y, parent=None)
         log.info(f"Read {len(self.key_index) - l0} regions from {stream!r}")
 
-    def load_csv(self, path):
-        with open(path, 'rt') as f:
-            self.read_csv(f)
-
-    def read_csv(self, file):
-        w = csv.reader(file)
-        h = next(w)
-        assert h == ['indent', 'names', 'kind', 'pop_mil', 'lat', 'lon', 'iana', 'gv_id']
-        stack = [None]
-        indent = 0
-        last = None
-
-        def a(x, sc=1.0, t=float):
-            return t(float(x) * sc) if (x is not None and x.strip() != "") else None
-        def b(x):
-            return int(x) if x is not None else None
-
-        for r in w:
-            i = len(r[0])
-            assert i % 4 == 0
-            r = r + ([None] * (len(h) - len(r)))
-            reg = Region(r[1].split('|'), kind=r[2], population=a(r[3], 1e6, int), lat=a(r[4]), lon=a(r[5]), iana=r[6], gleam_id=b(r[7]))
-            if i < indent:
-                for _ in range((indent - i) // 4):
-                    stack.pop()
-            elif i > indent:
-                assert i == indent + 4
-                stack.append(last)
-            else:  # i == ident
-                pass
-            #print("Add %r under %r" % (reg, stack[-1]))
-            self.add_region(reg, parent=stack[-1])
-            last = reg
-            indent = i
-
-    def write_csv(self, file=sys.stdout):
-        w = csv.writer(file)
-        w.writerow(['indent', 'names', 'kind', 'pop_mil', 'lat', 'lon', 'iana', 'gv_id'])
-        def f(x, mult=1.0):
-            return ("%.3f" % (x * mult)) if x is not None else None
-        def rec(reg, ind=0):
-            id = reg.gv_id if reg.kind == 'city' else None
-            t = [' ' * ind, "|".join(reg.names), reg.kind, f(reg.population, 1e-6), f(reg.lat), f(reg.lon), reg.iana, id]
-            t += reg.extra_csv
-            while len(t) > 4 and t[-1] is None:
-                t.pop()
-            w.writerow(t)
-            for i in reg.sub:
-                rec(i, ind + 4)
-        rec(self.root)
-
     def print_tree(self, file=sys.stdout, kinds=('region', 'continent', 'world')):
         def rec(reg, ind=0, parentpop=None):
             if parentpop:
@@ -298,8 +247,8 @@ class Regions:
 
         def rec(reg, w):
             if (kinds is None) or (reg.kind in kinds):
-                gv_id = reg.gv_id if reg.kind == 'city' else None
-                t = [reg.name, reg.kind, fi(reg.pop), f(reg.lat), f(reg.lon), gv_id]
+                gleam_id = reg.gleam_id if reg.kind == 'city' else None
+                t = [reg.name, reg.kind, fi(reg.pop), f(reg.lat), f(reg.lon), gleam_id]
                 for c in cols:
                     t.append(fi(reg.est.get(c)))
                 w.writerow(t)
--- test_scripts.sh
+++ test_scripts.sh
@@ -8,11 +8,19 @@ CSSEDIR="data/CSSE-COVID-19/csse_covid_19_data/csse_covid_19_time_series/"
 
 mkdir -p "$TMPDIR"
 
+echo
+echo "*** testing ./estimate.py ***"
+echo
+
 if [ -f "$FTDATA" ] && [ -d "$CSSEDIR" ] ; then
-    ./estimate.py data/example.xml -o "$TMPDIR/est.xml" -O "$TMPDIR/est.csv" -f "$FTDATA" -C "$CSSEDIR"
+    ./estimate.py data/definition-example.xml -o "$TMPDIR/est.xml" -O "$TMPDIR/est.csv" -f "$FTDATA" -C "$CSSEDIR"
 else
     echo "!!! Skipping estimate.py. Get $FTDATA and $CSSEDIR"
 fi
 
-./parameterize.py data/example.xml "$TMPDIR/params" -P 0.1,0.2,0 -P [1,0.1],-0.2,[0.5]
+echo
+echo "*** testing ./parameterize.py ***"
+echo
+
+./parameterize.py data/definition-example.xml "$TMPDIR/params" -P 0.1,0.2,0 -P [1,0.1],-0.2,[0.5]
 
