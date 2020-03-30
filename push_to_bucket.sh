d6358d0f63ca680630f962a2a1cdec4990409161
Daniel Hnyk <hnykda@users.noreply.github.com>
2020-03-30 21:51:51 +0200

    Update push_to_bucket.sh

--- push_to_bucket.sh
+++ push_to_bucket.sh
@@ -5,6 +5,6 @@ fi
 
 TGT="gs://static-covid/static/$2"
 
-gsutil -h "Cache-Control:public, max-age=10" cp -a public-read "$1" "$TGT"
+gsutil -h "Cache-Control:public, max-age=10" cp -Z -a public-read "$1" "$TGT"
 
 echo "URL: https://storage.googleapis.com/static-covid/static/$2"
